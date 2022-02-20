8206d310c798ba9dfa95cf635577f4ab3fd32a9d
lll903305160 <104660021+lll903305160@users.noreply.github.com>
2022-02-20 16:17:47 +0800

    爬虫xpath+apriori 关联规则挖掘

--- douban_apriori.py
+++ douban_apriori.py
@@ -21,3 +21,40 @@ def download(request_url):
     driver.get(request_url)
     time.sleep(1)
     html = driver.find_element_by_xpath("//*").get_attribute("outerHTML")
+    html = etree.HTML(html)
+    movie_lists = html.xpath("/html/body/div[@id='wrapper']/div[@id='root']/div[1]//div[@class='item-root']/div[@class='detail']/div[@class='title']/a[@class='title-text']")
+    name_lists = html.xpath("/html/body/div[@id='wrapper']/div[@id='root']/div[1]//div[@class='item-root']/div[@class='detail']/div[@class='meta abstract_2']")
+    #获取返回电影数据的个数
+    num = len(movie_lists)
+    if num>15:        #豆瓣搜索第一页会有16条数据，第一条是导演介绍需跳过
+        movie_lists = movie_lists[1:]
+        name_lists = name_lists[1:]
+    for movie,name_list in zip(movie_lists,name_lists):
+        if name_list.text is None:
+            continue
+        print(name_list.text)
+        names = name_list.text.split('/')
+        if names[0].strip() ==director and movie.text not in flags:
+            names[0] = movie.text #保留疑问
+            flags.append(movie.text)
+            csv_write.writerow(names)
+    print('OK')
+    print(num)
+    if num >= 14:
+        return True
+    else:
+        return False
+    
+
+start = 0
+while start < 10000:
+    request_url = base_url+str(start)
+    flag = download(request_url)
+    if flag:
+        start = start + 15
+    else:
+        break
+        
+out.close()
+print('finished')
+    
